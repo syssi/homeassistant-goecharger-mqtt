563f7fdac868cae3a070996671bc1fa9cddd3c7a
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-02-19 15:29:25 +0100

    Add first draft

--- custom_components/goecharger_mqtt/__init__.py
+++ custom_components/goecharger_mqtt/__init__.py
@@ -1,12 +1,18 @@
 """The go-eCharger (MQTT) integration."""
 from __future__ import annotations
 
+import logging
+
+from homeassistant.components import mqtt
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, ServiceCall, callback
+from homeassistant.helpers.typing import ConfigType
 
 from .const import DOMAIN
 
 PLATFORMS: list[str] = ["binary_sensor", "number", "sensor", "select", "switch"]
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -22,3 +28,15 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
 
     return unload_ok
+
+
+async def async_setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up integration."""
+
+    @callback
+    async def set_config_key_service(call: ServiceCall) -> None:
+        await mqtt.async_publish(hass, "test", call.data.get("value"))
+
+    hass.services.async_register(DOMAIN, "set_config_key", set_config_key_service)
+
+    return True
