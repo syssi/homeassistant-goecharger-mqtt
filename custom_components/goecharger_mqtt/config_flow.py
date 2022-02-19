56f65734d15866c90bb5fbf4a0225a74a843d0d7
Sebastian Muszynski <s.muszynski@constructiva.de>
2022-02-19 13:41:32 +0100

    Add first draft

--- custom_components/goecharger_mqtt/config_flow.py
+++ custom_components/goecharger_mqtt/config_flow.py
@@ -5,6 +5,7 @@ import logging
 from typing import Any
 
 from homeassistant import config_entries
+from homeassistant.components.mqtt import MqttServiceInfo
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
@@ -15,11 +16,13 @@ from .const import CONF_SERIAL_NUMBER, CONF_TOPIC_PREFIX, DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
+DEFAULT_NAME = "go-eCharger"
+DEFAULT_TOPIC_PREFIX = "/go-eCharger"
 
 STEP_USER_DATA_SCHEMA = vol.Schema(
     {
         vol.Required(CONF_SERIAL_NUMBER): vol.All(cv.string, vol.Length(min=6, max=6)),
-        vol.Optional(CONF_TOPIC_PREFIX, default="/go-eCharger"): cv.string,
+        vol.Optional(CONF_TOPIC_PREFIX, default=DEFAULT_TOPIC_PREFIX): cv.string,
     }
 )
 
@@ -51,13 +54,52 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
     if not await hub.validate_device_topic():
         raise CannotConnect
 
-    return {"title": f"go-eCharger {serial_number}"}
+    return {"title": f"{DEFAULT_NAME} {serial_number}"}
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for go-eCharger (MQTT)."""
 
     VERSION = 1
+
+    def __init__(self) -> None:
+        """Initialize flow."""
+        self._serial_number = None
+
+    async def async_step_mqtt(self, discovery_info: MqttServiceInfo) -> FlowResult:
+        """Handle a flow initialized by MQTT discovery."""
+        subscribed_topic = discovery_info.subscribed_topic
+
+        # Subscribed topic must be in sync with the manifest.json
+        assert subscribed_topic == "/go-eCharger/+/var"
+
+        # Example topic: /go-eCharger/072246/var
+        topic = discovery_info.topic
+        (prefix, suffix) = subscribed_topic.split("+", 2)
+        self._serial_number = topic.replace(prefix, "").replace(suffix, "")
+
+        if not self._serial_number.isnumeric():
+            return self.async_abort(reason="invalid_discovery_info")
+
+        await self.async_set_unique_id(self._serial_number)
+        self._abort_if_unique_id_configured()
+
+        return await self.async_step_mqtt_confirm()
+
+    async def async_step_mqtt_confirm(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Confirm the setup."""
+        data = {
+            CONF_SERIAL_NUMBER: self._serial_number,
+            CONF_TOPIC_PREFIX: DEFAULT_TOPIC_PREFIX,
+        }
+        title = f"{DEFAULT_NAME} {self._serial_number}"
+
+        if user_input is not None:
+            return self.async_create_entry(title=title, data=data)
+
+        return self.async_show_form(step_id="mqtt_confirm")
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
