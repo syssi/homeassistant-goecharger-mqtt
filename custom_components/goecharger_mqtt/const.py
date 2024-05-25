6d8058b37d7da24482c51824a1de00e30b642f75
Kryštof Korb <krystof@korb.cz>
2024-05-25 09:08:23 +0200

    Remove leading slash from `DEFAULT_TOPIC_PREFIX` (#128)

--- custom_components/goecharger_mqtt/const.py
+++ custom_components/goecharger_mqtt/const.py
@@ -9,7 +9,7 @@ ATTR_VALUE = "value"
 CONF_SERIAL_NUMBER = "serial_number"
 CONF_TOPIC_PREFIX = "topic_prefix"
 
-DEFAULT_TOPIC_PREFIX = "/go-eCharger"
+DEFAULT_TOPIC_PREFIX = "go-eCharger"
 
 DEVICE_INFO_MANUFACTURER = "go-e"
 DEVICE_INFO_MODEL = "go-eCharger HOME"
