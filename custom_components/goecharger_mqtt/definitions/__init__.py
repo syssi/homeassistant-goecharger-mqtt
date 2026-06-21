ea1aec2a7f04b7d87a8cacc65b7756ea536b6475
Sebastian Muszynski <basti@linkt.de>
2026-06-21 17:01:51 +0200

    Add translation_key field to GoEChargerEntityDescription

--- custom_components/goecharger_mqtt/definitions/__init__.py
+++ custom_components/goecharger_mqtt/definitions/__init__.py
@@ -20,3 +20,4 @@ class GoEChargerEntityDescription(EntityDescription):
     domain: str = "generic"
     disabled: bool | None = None
     disabled_reason: str | None = None
+    translation_key: str | None = None
